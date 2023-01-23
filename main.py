f3ab000abf1f090799a483365421ea82ba39808a
Jorge Caballeros Pérez <cab20009@uvg.edu.gt>
2023-01-23 13:32:34 -0600

    .

---
+++ main.py
@@ -0,0 +1,15 @@
+import modules as mod
+from PIL import Image
+
+#TODO change this to input
+PATH = "Images\lab1.bmp"
+im = Image.open(PATH)
+px = im.load()
+
+for i in 
+    
+
+
+
+#END
+im.show()
---
+++ modules.py
@@ -0,0 +1,11 @@
+from PIL import Image
+
+
+# def getImage(image):
+#     if '.bmp' in image:
+#         im = Image.open(image)
+#     else:
+#         #image = str(input("Ingrese el PATH o nombre de imagen: \n"))
+#         im = Image.open(image).save("output\Result.bmp")
+#     return im
+
