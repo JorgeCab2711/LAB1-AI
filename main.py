65d7f63153cbb8b8bfdadbb52074ac08703d65c5
Jorge Caballeros Pérez <cab20009@uvg.edu.gt>
2023-01-24 22:10:56 -0600

    .

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ from PIL import Image
 #TODO change this to input
 PATH = "Images\lab1.bmp"
 im = Image.open(PATH)
-px = im.load()
 
-for i in 
     
+pixels = list(im.getdata())
+width, height = im.size
+pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
+
+pixel_cords = []
+start = []
 
 
+
+
+# for pixel in pixels:
+#     if mod.get_color(pixel) == ('green'):
+#         start.append(pixel)
+        
 
 #END
-im.show()
+#im.show()
--- modules.py
+++ modules.py
@@ -1,11 +1,15 @@
-from PIL import Image
 
 
-# def getImage(image):
-#     if '.bmp' in image:
-#         im = Image.open(image)
-#     else:
-#         #image = str(input("Ingrese el PATH o nombre de imagen: \n"))
-#         im = Image.open(image).save("output\Result.bmp")
-#     return im
-
+#Gets the color from the pixel range in a general range
+def get_color(color):
+    r,g,b = color
+    if g < r/2 and b < r/2:
+        return "red"
+    elif r < g/2 and b < g/2:
+        return "green"
+    elif r < b/2 and g < b/2:
+        return "blue"
+    elif r < 50 and g < 50 and b < 50:
+        return 'black'
+    elif r < 200 and g < 200 and b < 200:
+        return 'black'
