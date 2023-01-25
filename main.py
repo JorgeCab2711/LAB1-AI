9a6be28ffbc55d37d8e5f2ff11098d913137524a
JorgeCab2711 <cab20009@uvg.edu.gt>
2023-01-24 23:33:07 -0600

    .

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import modules as mod
 from PIL import Image
 
-#TODO change this to input
-PATH = "Images\lab1.bmp"
+# TODO change this to input
+PATH = "Images\\test.bmp"
 im = Image.open(PATH)
 
-    
-pixels = list(im.getdata())
+# Setting and normalizing coordinates
 width, height = im.size
-pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
 
-pixel_cords = []
+# pixel coordinates
+pixel_coords = im.load()
 start = []
+end = []
+wall = []
+
+# cami_pixel = ((x,y), (R,G,B))
+# Iterarar la lista de pixeles y agregar todas sus caracteristicas en una lista de tuplas
+cam_pixels = []
+for x in range(width):
+    for y in range(height):
+        cam_pixels.append(((x, y), (pixel_coords[x, y])))
+
+# obterner los pixeles que tienen color verde, estos marcaran los puntos de inicio
+for i in cam_pixels:
+    gamma = mod.get_color(i[1])
+    if gamma == 'green':
+        start.append(i[0])
+        print(gamma)
+
+# obterner los puntos
+for i in cam_pixels:
+    gamma = mod.get_color(i[1])
+    if gamma == 'green':
+        start.append(i[0])
+        print(gamma)
 
 
-
-
-# for pixel in pixels:
-#     if mod.get_color(pixel) == ('green'):
-#         start.append(pixel)
-        
-
-#END
-#im.show()
+# END
+# im.show()
